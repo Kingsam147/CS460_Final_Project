c05a5a1827c97efeb6826ecd431ed969156a7a40
Kingsam147 <Samueldarius1470@gmail.com>
2024-12-21 19:38:03 -0500

    final product

--- createCards.py
+++ createCards.py
@@ -1,6 +1,6 @@
 import json 
-import os
 
+# method to create a card
 def createCard(image: str, name: str):
      card = {
           "image": image,
@@ -9,20 +9,22 @@ def createCard(image: str, name: str):
      return card
 
 def main(): 
-    cards = [
+
+     # continuously calling the method to create a list of ten unique cards
+     cards = [
         createCard('resized-images/chopper.jpg', 'cardOne'),
         createCard('resized-images/download.jpg', 'cardTwo'),
         createCard('resized-images/Duke_Dennis.jpg', 'cardThree'),
         createCard('resized-images/IMG_0399.jpg', 'cardFour'),
-        createCard('resized-images/IMG_9253.jpg', 'cardFive'),
-        createCard('resized-images/IMG_9346.jpg', 'cardSix'),
-        createCard('resized-images/Lebron_Sunshine.jpg', 'cardSeven'),
-        createCard('resized-images/Luffy.jpg', 'cardEight'),
-        createCard('resized-images/shooters_gonna_shoot.jpg', 'cardNine'),
-        createCard('resized-images/Sigma_Man.jpg', 'cardTen') 
-    ] 
+        createCard('resized-images/IMG_9346.jpg', 'cardFive'),
+        createCard('resized-images/Lebron_Sunshine.jpg', 'cardSix'),
+        createCard('resized-images/Luffy.jpg', 'cardSeven'),
+        createCard('resized-images/shooters_gonna_shoot.jpg', 'cardEight'),
+        createCard('resized-images/Sigma_Man.jpg', 'cardNine') 
+     ] 
 
-    with open("data.json", "w") as f: 
+     # then creating a .json file to store those cards
+     with open("cards.json", "w") as f: 
          json.dump(cards, f, indent=2)
 
 if __name__ == "__main__":
